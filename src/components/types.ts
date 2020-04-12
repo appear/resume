9dd78c1735f3c2a0b764ea883985c3971eb881db
appear <appear.ko@gmail.com>
2020-04-12 09:50:53 +0900

    :zap: feature: data json type 추가

--- src/components/types.ts
+++ src/components/types.ts
@@ -3,3 +3,39 @@ export type MetaData = {
   introduce: string
   author: string
 }
+
+export type ContactType = 'github' | 'phone' | 'mail' | 'blog' | 'linkedIn'
+
+export type Contact = {
+  [key in ContactType]?: string
+}
+
+export type Project = {
+  name: string
+  start_date: string
+  end_date: string
+  summary: string
+  link: string
+  experiences: string[]
+  tags: string[]
+}
+
+export type Career = {
+  name: string
+  summary: string
+  department: string
+  start_date: string
+  ent_date: string
+  position: string
+  projects: Project[]
+}
+
+export type Content = {
+  title: string
+  description: string
+}
+
+export type Category = {
+  name: string
+  contents: Content[]
+}
